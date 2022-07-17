6f10fcfd68114032f526b5e47d27059e209e2f66
Aesycos <aesycos@protonmail.com>
2022-07-17 07:56:34 -0500

    Update revert.sh

--- revert.sh
+++ revert.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Minecraft Server Permissions Fix Script - James A. Chambers - https://jamesachambers.com/minecraft-bedrock-edition-ubuntu-dedicated-server-guide/
 
 # Set version in version_pin.txt
 # Reverts version pinning if you set a custom version to avoid updates being out of sync with Microsoft's servers
@@ -14,5 +13,5 @@ else
     echo "Unable to set path variable.  You likely need to download an updated version of SetupMinecraft.sh from GitHub!"
 fi
 
-ls -r1 dirname/minecraftbe/servername/downloads/ | grep bedrock-server | head -2 | tail -1 >version_pin.txt
-echo "Set previous version in version_pin.txt: $(cat dirname/minecraftbe/servername/version_pin.txt)"
+ls -r1 dirname/minecraft/servername/downloads/ | grep bedrock-server | head -2 | tail -1 >version_pin.txt
+echo "Set previous version in version_pin.txt: $(cat dirname/minecraft/servername/version_pin.txt)"
